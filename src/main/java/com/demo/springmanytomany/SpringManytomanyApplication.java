920e6bdfa4ab33ca05c5809d8bec51ef33a75652
Capdoo <mazher_2014@hotmail.com>
2023-02-23 21:00:10 -0500

    first commit

---
+++ src/main/java/com/demo/springmanytomany/SpringManytomanyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringManytomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringManytomanyApplication.class, args);
+	}
+
+}
